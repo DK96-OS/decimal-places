468091647edb3e10e21ca2a219b2190194e077dd
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-09-12 15:01:50 -0400

    Initialize Project (#1)
    Build Updates:
    * Create digits/build.gradle
    * Update build.gradle - set package url
    * Update settings.gradle - set project name, add dokka plugin version, add digits module
    
    Digits Main:
    * Create digits/src/main/java/decimalplaces/digits/DigitArray.kt
    
    Digits Test:
    * Create digits/src/test/java/decimalplaces/digits/DigitArrayTest.kt
    
    GitHub Workflows:
    * Create dependabot.yml
    * Create publish_release.yml

---
+++ digits/src/main/java/decimalplaces/digits/DigitArray.kt
@@ -0,0 +1,105 @@
+package decimalplaces.digits
+
+/** Manages an Array of Digits.
+ */
+class DigitArray(
+    /** An array of digit values.
+     */
+    val digits: ByteArray
+) {
+
+    operator fun get(index: Int): Byte {
+        require(index >= 0 && index < digits.size)
+        return digits[index]
+    }
+
+    override fun toString()
+        : String = digits.joinToString("")
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is DigitArray) return false
+        return this.digits.contentEquals(other.digits)
+    }
+
+    override fun hashCode()
+        : Int = digits.contentHashCode()
+
+    operator fun plus(other: DigitArray): DigitArray {
+        val newSize = maxOf(digits.size, other.digits.size)
+        val result = ByteArray(newSize) {
+            (digits[it] + other.digits[it]).toByte()
+        }
+        for (i in newSize - 1 downTo 1) {
+            val digitValue = result[i]
+            if (digitValue > 9) {
+                result[i - 1] = (result[i - 1] + digitValue / 10).toByte()
+                result[i] = (digitValue % 10).toByte()
+            }
+        }
+        if (result[0] > 9) {
+            val carryOverflow = (result[0] / 10)
+            result[0] = (result[0] % 10).toByte()
+            return DigitArray(
+                ByteArray(newSize + 1) {
+                    when (it) {
+                        0 -> carryOverflow.toByte()
+                        else -> result[it - 1]
+                    }
+                }
+            )
+        }
+        return DigitArray(result)
+    }
+
+    operator fun minus(other: DigitArray): DigitArray {
+        val newSize = maxOf(digits.size, other.digits.size)
+        val result = ByteArray(newSize) {
+            if (it < digits.size) digits[it] else 0
+        }
+        // Perform subtraction digit by digit from right to left
+        for (i in newSize - 1 downTo 1) {
+            val currentDigitValue = result[i]
+            val otherDigitValue = if (i < other.digits.size)
+                other.digits[i] else 0
+            // If the current digit value is less than the other digit value, borrow from next position
+            if (currentDigitValue < otherDigitValue) {
+                var borrowPosition = i - 1
+                while (result[borrowPosition] < 1)
+                    if (--borrowPosition < 0)
+                        throw ArithmeticException("Cannot subtract greater value")
+                // Borrow from digit and subtract it from the current position
+                result[borrowPosition] = result[borrowPosition].dec()
+                while (i - borrowPosition > 0) {
+                    result[++borrowPosition] = 9
+                }
+                result[i] = (10 + currentDigitValue - otherDigitValue).toByte()
+            } else {
+                result[i] = (currentDigitValue - otherDigitValue).toByte()
+            }
+        }
+        val firstValue = result[0]
+        val diff = firstValue - other.digits[0]
+        if (firstValue < 0 || diff < 0) {
+            throw IllegalStateException()
+        }
+        result[0] = diff.toByte()
+        return DigitArray(result)
+    }
+
+    /** Remove the Trailing Zeros at the end of the Array.
+     */
+    fun trimTrailingZeros()
+        : DigitArray {
+        val initialSize = digits.size - 1
+        for (trimIndex in initialSize downTo 1) {
+            if (digits[trimIndex] != 0.toByte()) {
+                return if (trimIndex < initialSize) {
+                    DigitArray(digits.copyOf(trimIndex + 1))
+                } else
+                    this
+            }
+        }
+        return DigitArray(byteArrayOf(0))
+    }
+
+}
---
+++ digits/src/test/java/decimalplaces/digits/DigitArrayTest.kt
@@ -0,0 +1,175 @@
+package decimalplaces.digits
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class TestDigitArray {
+
+    private lateinit var mInstance: DigitArray
+
+    @BeforeEach
+    fun testSetup() {
+        val digits1 = byteArrayOf(0, 2, 5)
+        val digits2 = byteArrayOf(3, 6, 9)
+        mInstance = DigitArray(digits1) + DigitArray(digits2)
+    }
+
+    @Test
+    fun accessDigits() {
+        assertEquals(mInstance[0], 3.toByte())
+        assertEquals(mInstance[1], 9.toByte())
+        assertEquals(mInstance[2], 4.toByte())
+    }
+
+    @Test
+    fun toString_InitialCondition_() {
+        assertEquals(
+            "394", mInstance.toString()
+        )
+    }
+
+    @Test
+    fun equals_InitialCondition_Self_ReturnsTrue() {
+        assertEquals(mInstance, mInstance)
+    }
+
+    @Test
+    fun equals_InitialCondition_SameValues_ReturnsTrue() {
+        assertEquals(
+            DigitArray(byteArrayOf(3, 9, 4)),
+            mInstance
+        )
+    }
+
+    @Test
+    fun equals_InitialCondition_DifferentValue1_ReturnsFalse() {
+        assertNotEquals(
+            DigitArray(byteArrayOf(2, 9, 4)),
+            mInstance
+        )
+    }
+
+    @Test
+    fun equals_InitialCondition_DifferentValue2_ReturnsFalse() {
+        assertNotEquals(
+            DigitArray(byteArrayOf(3, 7, 4)),
+            mInstance
+        )
+    }
+
+    @Test
+    fun addArrays() {
+        val expected = byteArrayOf(3, 9, 4)
+        for (i in 0 until mInstance.digits.size) {
+            assertEquals(mInstance[i], expected[i])
+        }
+    }
+
+    @Test
+    fun testMinus_Simple1_ReturnsValid() {
+        val simple1 = DigitArray(byteArrayOf(1))
+        val simple2 = DigitArray(byteArrayOf(2))
+        val result = simple2 - simple1
+        assertEquals(1, result.digits[0])
+    }
+
+    @Test
+    fun testMinus_InitialCondition_Zero_ReturnsInitialCondition() {
+        val zeroInput = DigitArray(byteArrayOf(0))
+        assertEquals(mInstance, mInstance - zeroInput)
+    }
+
+    @Test
+    fun testMinus_InitialCondition_Zero2_ReturnsInitialCondition() {
+        val zeroInput = DigitArray(byteArrayOf(0, 0))
+        assertEquals(mInstance, mInstance - zeroInput)
+    }
+
+    @Test
+    fun testMinus_InitialCondition_Zero4_ReturnsLength4WithTrailingZero() {
+        val zeroInput = DigitArray(byteArrayOf(0, 0, 0, 0))
+        assertEquals(
+            DigitArray(byteArrayOf(3, 9, 4, 0)),
+            mInstance - zeroInput
+        )
+    }
+
+    @Test
+    fun testMinus_() {
+        val subtractOperand = DigitArray(byteArrayOf(1, 2, 3))
+        val result = mInstance - subtractOperand
+        assertEquals(2, result.digits[0])
+        assertEquals(7, result.digits[1])
+        assertEquals(1, result.digits[2])
+    }
+
+    @Test
+    fun testMinus_2() {
+        val subtractOperand = DigitArray(byteArrayOf(1, 2, 4))
+        val result = mInstance - subtractOperand
+        assertEquals(2, result.digits[0])
+        assertEquals(7, result.digits[1])
+        assertEquals(0, result.digits[2])
+    }
+
+    @Test
+    fun testMinus_3() {
+        val subtractOperand = DigitArray(byteArrayOf(1, 2, 5))
+        val result = mInstance - subtractOperand
+        assertEquals(2, result.digits[0])
+        assertEquals(6, result.digits[1])
+        assertEquals(9, result.digits[2])
+    }
+
+    @Test
+    fun testMinus_Borrowing_ReturnsValidDifference() {
+        mInstance = DigitArray(byteArrayOf(1, 0, 4))
+        val subtractOperand = DigitArray(byteArrayOf(0, 2, 5))
+        val result = mInstance - subtractOperand
+        assertEquals(0, result.digits[0])
+        assertEquals(7, result.digits[1])
+        assertEquals(9, result.digits[2])
+    }
+
+    @Test
+    fun trimTrailingZeros_() {
+        mInstance = DigitArray(byteArrayOf(1, 0))
+        mInstance = mInstance.trimTrailingZeros()
+        assertEquals(
+            1, mInstance.digits.size
+        )
+    }
+
+    @Test
+    fun trimTrailingZeros_2() {
+        mInstance = DigitArray(byteArrayOf(1, 0, 2, 0))
+        mInstance = mInstance.trimTrailingZeros()
+        assertEquals(
+            3, mInstance.digits.size
+        )
+        assertEquals(
+            1, mInstance.digits[0]
+        )
+        assertEquals(
+            0, mInstance.digits[1]
+        )
+        assertEquals(
+            2, mInstance.digits[2]
+        )
+    }
+
+    @Test
+    fun trimTrailingZeros_AllZero3() {
+        mInstance = DigitArray(byteArrayOf(0, 0, 0))
+        mInstance = mInstance.trimTrailingZeros()
+        assertEquals(
+            1, mInstance.digits.size
+        )
+        assertEquals(
+            0, mInstance.digits[0]
+        )
+    }
+
+}
