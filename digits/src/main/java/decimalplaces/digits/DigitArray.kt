37ad47c03916cc0654eab2ba9f104928a6579ace
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-09-24 09:27:30 -0400

    Digits Main: * Update DigitArray - modify method plus to use Positive Lead Digit Overflow, add method isLeadDigitOverflowing, add method collectOverflowFromLeadDigit, modfiy method minus to use Negative Lead Digit Overflow, add method findBorrowableIndex

--- digits/src/main/java/decimalplaces/digits/DigitArray.kt
+++ digits/src/main/java/decimalplaces/digits/DigitArray.kt
@@ -50,59 +50,73 @@ class DigitArray(
                 result[i] = (digitValue % 10).toByte()
             }
         }
-        if (result[0] > 9) {
-            val carryOverflow = (result[0] / 10)
-            result[0] = (result[0] % 10).toByte()
-            return DigitArray(
-                ByteArray(newSize + 1) {
-                    when (it) {
-                        0 -> carryOverflow.toByte()
-                        else -> result[it - 1]
-                    }
+        return DigitArray(result)
+    }
+
+    fun isLeadDigitOverflowing(
+        results: ByteArray = digits,
+    ) : Boolean {
+        return results[0] > 9 || results[0] < 0
+    }
+
+    /** Obtain the Overflow Value from the Lead Digit.
+     */
+    fun collectOverflowFromLeadDigit(
+        results: ByteArray = digits,
+    ) : Byte {
+        val leadDigit = results[0]
+        if (leadDigit < 0) { // Lead Digit is Negative
+            results[0] = (10 - ((leadDigit * -1) % 10)).toByte()
+            return ((leadDigit) / 10).toByte()
+        }
+        results[0] = (leadDigit % 10).toByte()
+        return (leadDigit / 10).toByte()
+    }
+
+    operator fun minus(other: DigitArray): DigitArray {
+        val result = ByteArray(maxOf(digits.size, other.digits.size)) {
+            if (it < digits.size) digits[it] else 0
+        }
+        for (i in result.size - 1 downTo 0) {
+            val otherDigitValue = if (i < other.digits.size) other.digits[i] else 0
+            if (otherDigitValue < 0 || otherDigitValue > 9)
+                throw IllegalArgumentException("Other DigitArray contains non-normalized digits.")
+            if (otherDigitValue == 0.toByte())
+                continue
+            val diff = result[i] - otherDigitValue
+            result[i] = (if (diff < 0) {
+                val borrowIndex = findBorrowableIndex(i - 1, result)
+                if (-1 < borrowIndex) {
+                    result[borrowIndex] = result[borrowIndex].dec()
+                    for (j in borrowIndex + 1 until i)
+                        result[j] = 9
+                    10 + diff
+                } else if (i == 0) diff - 10 else {
+                    // Negative Lead Digit Overflow
+                    result[0] = (-11).toByte()
+                    for (j in 1 until i)
+                        result[j] = 9
+                    10 + diff
                 }
-            )
+            } else diff).toByte()
         }
         return DigitArray(result)
     }
 
-    operator fun minus(other: DigitArray): DigitArray {
-        val newSize = maxOf(digits.size, other.digits.size)
-        val result = ByteArray(newSize) {
-            if (it < digits.size) digits[it] else 0
+    /** Find an index that can be borrowed from.
+     *  Returns the largest index below the start index where the value is greater than zero.
+     *  @param startIndex The first index in the search. This index will be checked first.
+     *  @param digitArray The Array of Digits.
+     *  @return The index of the first digit that can be borrowed from, given the start index.
+     */
+    fun findBorrowableIndex(
+        startIndex: Int,
+        digitArray: ByteArray = digits,
+    ) : Int {
+        for (i in startIndex downTo  0) {
+            if (digitArray[i] > 0) return i // Can be borrowed from
         }
-        // Perform subtraction digit by digit from right to left
-        for (i in newSize - 1 downTo 1) {
-            val currentDigitValue = result[i]
-            val otherDigitValue = if (i < other.digits.size)
-                other.digits[i] else 0
-            // If the current digit value is less than the other digit value, borrow from next position
-            if (currentDigitValue < otherDigitValue) {
-                var borrowPosition = i - 1
-                while (result[borrowPosition] < 1)
-                    if (--borrowPosition < 0)
-                        throw ArithmeticException("Cannot subtract greater value")
-                // Borrow from digit and subtract it from the current position
-                result[borrowPosition] = result[borrowPosition].dec()
-                while (i - borrowPosition > 0) {
-                    result[++borrowPosition] = 9
-                }
-                result[i] = (10 + currentDigitValue - otherDigitValue).toByte()
-            } else {
-                result[i] = (currentDigitValue - otherDigitValue).toByte()
-            }
-        }
-        val firstValue = result[0]
-        val diff = firstValue - other.digits[0]
-        if (firstValue < 0 || diff < 0) {
-            result[0] = (10 + diff).toByte()
-            val newArray = ByteArray(newSize + 1) {
-                if (it == 0) -1 else result[it - 1]
-            }
-            return DigitArray(newArray)
-        } else {
-            result[0] = diff.toByte()
-            return DigitArray(result)
-        }
+        return -1
     }
 
     /** Remove the Trailing Zeros at the end of the Array.
