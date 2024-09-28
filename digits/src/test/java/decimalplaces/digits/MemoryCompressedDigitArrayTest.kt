a05d838d9f573ef55d246c2537ef9eb8f02dbc63
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-09-28 19:03:39 -0400

    Digits Test: * Update MemoryCompressedDigitArrayTest - add test cases

--- digits/src/test/java/decimalplaces/digits/MemoryCompressedDigitArrayTest.kt
+++ digits/src/test/java/decimalplaces/digits/MemoryCompressedDigitArrayTest.kt
@@ -1,9 +1,11 @@
 package decimalplaces.digits
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class MemoryCompressedDigitArrayTest {
 
@@ -101,6 +103,46 @@ class MemoryCompressedDigitArrayTest {
     }
 
     @Test
+    fun testSet_Index0_Value0_UpdatesDigits() {
+        mInstance[0] = 0
+        assertEquals("062", mInstance.toString())
+    }
+
+    @Test
+    fun testSet_Index1_Value0_UpdatesDigits() {
+        mInstance[1] = 0
+        assertEquals("402", mInstance.toString())
+    }
+
+    @Test
+    fun testSet_NegativeIndex_ThrowsIndexOutOfBoundsException() {
+        assertThrows<IndexOutOfBoundsException> {
+            mInstance[-1] = 5
+        }
+    }
+
+    @Test
+    fun testSet_IndexOutOfBounds_ThrowsIndexOutOfBoundsException() {
+        assertThrows<IndexOutOfBoundsException> {
+            mInstance[10] = 5
+        }
+    }
+
+    @Test
+    fun testSet_Index0_NegativeValue_ThrowsIllegalArgumentsException() {
+        assertThrows<IllegalArgumentException> {
+            mInstance[0] = -1
+        }
+    }
+
+    @Test
+    fun testSet_Index0_LargeValue_ThrowsIllegalArgumentsException() {
+        assertThrows<IllegalArgumentException> {
+            mInstance[0] = 20
+        }
+    }
+
+    @Test
     fun testToString_InitialCondition_SameAsRegularDigitArray() {
         assertEquals(
             regularDigitArray.toString(),
@@ -115,6 +157,49 @@ class MemoryCompressedDigitArrayTest {
         assertEquals(
             regularDigitArray.toString(),
             mInstance.toString()
+        )
+    }
+
+    @Test
+    fun testConstructor_LeadDigitOverflow_ThrowsIllegalArgumentException() {
+        val overflowingDigitArray = regularDigitArray + DigitArray(byteArrayOf(9, 9, 9))
+        assertThrows<IllegalArgumentException> {
+            MemoryCompressedDigitArray(overflowingDigitArray)
+        }
+    }
+
+    @Test
+    fun testEquals_InitialCondition_SameDigitArray_ReturnsFalse() {
+        assertNotEquals(mInstance, regularDigitArray)
+    }
+
+    @Test
+    fun testEquals_InitialCondition_SimilarMemoryCompressedDigitArray_ReturnsTrue() {
+        assertEquals(
+            mInstance, MemoryCompressedDigitArray(regularDigitArray)
+        )
+    }
+
+    @Test
+    fun testEquals_InitialCondition_DifferentDigitCount_ReturnsTrue() {
+        regularDigitArray = DigitArray(byteArrayOf(5, 4, 3, 2, 1))
+        assertNotEquals(
+            mInstance, MemoryCompressedDigitArray(regularDigitArray)
+        )
+    }
+
+    @Test
+    fun testEquals_InitialCondition_DifferentDigitValue_ReturnsFalse() {
+        regularDigitArray.digits[1] = 0
+        assertNotEquals(
+            mInstance, MemoryCompressedDigitArray(regularDigitArray)
+        )
+    }
+
+    @Test
+    fun testHashCode_InitialCondition_ReturnsStable() {
+        assertEquals(
+            3163, mInstance.hashCode()
         )
     }
 
