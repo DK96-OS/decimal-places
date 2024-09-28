13728fa0afad88eb7d392ee12088a2d08f0bcb65
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-09-28 19:49:34 -0400

    Digits Test: * Update DigitArrayTest - rename test class

--- digits/src/test/java/decimalplaces/digits/DigitArrayTest.kt
+++ digits/src/test/java/decimalplaces/digits/DigitArrayTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class TestDigitArray {
+class DigitArrayTest {
 
     private lateinit var mInstance: DigitArray
 
